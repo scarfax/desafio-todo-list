4244d5edd9345cfe0a455a9962ebe39d0d4e17f0
Sergio Campos <scarfax@gmail.com>
2022-11-16 16:25:37 -0300

    Adicionando versão 0.8

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import './global.css';
+import './styles.css';
+import { useState } from 'react';
 
 function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  function handleCreateTask() {
+    if (task === '') {
+      alert('Digite alguma tarefa');
+    } else {
+      const idRandom = (num) => Math.floor(Math.random() * num);
+      const newTask = { id: idRandom(102343), title: task, isComplete: false };
+
+      setTasks([...tasks, newTask]);
+      setTask('');
+    }
+  }
+
+  function handleToggleTaskCompletion(id) {
+    const taskComplete = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, isComplete: !task.isComplete };
+      }
+
+      return task;
+    });
+
+    setTasks(taskComplete);
+  }
+
+  function handleDeleteTask(id) {
+    setTasks(tasks.filter((remove) => remove.id !== id));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo">
+        <h1>Lista de tarefas</h1>
+        <header>
+          <input
+            type="text"
+            value={task}
+            onChange={(event) => setTask(event.target.value)}
+          />
+          <button onClick={handleCreateTask}>Adicionar</button>
+          {tasks.map((task) => (
+            <div
+              key={task.id}
+              className={
+                task.isComplete ? 'task-container completed' : 'task-container'
+              }
+            >
+              <div className="check-and-title">
+                <label className="checkbox-container">
+                  <input
+                    type="checkbox"
+                    onClick={() => handleToggleTaskCompletion(task.id)}
+                  />
+                  <span className="checkmark"></span>
+                </label>
+                <p>{task.title}</p>
+              </div>
+              <div>
+                <button
+                  className="button-delete"
+                  onClick={() => handleDeleteTask(task.id)}
+                >
+                  Apagar
+                </button>
+              </div>
+            </div>
+          ))}
+        </header>
+      </div>
     </div>
   );
 }
